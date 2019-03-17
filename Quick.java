51fb87f61af15ec7bee44ec6458ece64f1b2f38e
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-17 11:49:40 -0400

    Update Quick.java

--- Quick.java
+++ Quick.java
@@ -1,16 +1,9 @@
 public class Quick{
 
-	public static void main(String[] args){
-		int[] data = {1, 3, 5, 6, 2, 3, 8, 9};
-		for(int i = 0; i < data.length; i++){
-			System.out.print(data[i] + " ");
-		}
-		System.out.println();
-		partition(data, 0, 8);
-		for(int j = 0; j < data.length; j++){
-			System.out.print(data[j] + " ");
-		}
-		//System.out.println(quickselect(data, 5));
+	public static void swap(int[] data, int a, int b){
+		int temp = data[a];
+		data[a] = data[b];
+		data[b] = temp;
 	}
 	/*Modify the array such that:
  *1. Only the indices from start to end inclusive are considered in range
@@ -20,45 +13,59 @@ public class Quick{
  *4. all elements in range that are larger than the pivot element are placed after the pivot element.
  *@return the index of the final position of the pivot element.
  */
-	public static int partition (int [] data, int start, int end){
-		int rand = (int) (start + (end - start) * Math.random());
-		int pivot = data[rand];
-		int temp1 = data[rand];
-		data[rand] = data[start];
-		data[start] = temp1;//swap the first element with the pivot
-		int place = start;
-		for(int i = 0; i < data.length; i++){
-			if(data[i] <= pivot){
-				int temp = data[i];
-				data[i] = data[place];
-				data[place] = temp;
-				place++;
-			}
+	public static int partition(int[] data, int start, int end){
+      int piv = (int)(Math.random() * (end - start)) + start;
+      if (piv != start){//make sure pivot is at the start, and update the index of the pivot
+        swap(data,start,piv);
+        piv = start;
+        start++;//we're sorting stuff after the pivot
+      }
+      while (start != end){//we're gonna use start and end to control the boundaries
+        if (data[start] > data[piv]){
+          swap(data,start,end);
+          end--;//if it's greater than the pivot, move it to the end and adjust the bound accordingly
+        }
+        else{//otherwise we don't need to do anything so we move on
+          start++;
+        }
+      }
+      if (data[start] <= data[piv]){
+       swap(data,piv,start);
+       piv = start;//if it's less than or equal to the pivot, move it to the start and adjust the bound accordingly
+      }
+      else {
+       swap(data,piv,start-1);//otherwise we move the pivot back
+       piv = start-1;
+      }
 
-			for(int j = 0; j < data.length; j++){
-				System.out.print(data[j] + " ");
-			}
-
-			System.out.println();
-		}
-		int temp2 = data[place-1];
-		data[place-1] = pivot;
-		data[start] = temp2;
-		return place;
-	}
+      return piv;
+    }
 
 	public static int quickselect(int[] data, int k){
-		return quickselectH(data, k, 0, data.length - 1);
+		int lo = 0;
+		int hi = data.length - 1;//initialize your lo and hi
+	  	int piv = partition(data, lo, hi);
+  		while(piv != k){//we stop when we know index k is in the right spot
+    	if (k < piv){
+      		hi = piv - 1;//we zero in from above
+    	}
+
+	    if (k > piv){
+      		lo = piv+1; //we zero in from below
+    	}
+    		piv = partition(data,lo,hi);
+  		}
+  		return data[k];
 	}
-	public static int quickselectH(int[] data, int k, int start, int end){
-		if(start == end) return data[k];
+	
+	public static void quicksort(int[] data){
+		quicksortH(data, 0, data.length - 1);
+	}
+
+	public static void quicksortH(int[] data, int start, int end){
+		if(start >= end) return;
 		int piv = partition(data, start, end);
-		if(piv < k){
-			return quickselectH(data, k, piv+1, start);
-		}
-		if(piv > k){
-			return quickselectH(data, k, end, piv);
-		}
-		return data[piv];
+		quicksortH(data, start, piv - 1);
+		quicksortH(data, piv + 1, end);
 	}
 }
